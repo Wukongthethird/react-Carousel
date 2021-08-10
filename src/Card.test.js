2388bec813ad0c8f481f5afd87863c4117e5d675
Wukongthethird <simonzhang9518@gmail.com>
2021-08-10 15:43:12 -0700

    Card test

---
+++ src/Card.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Card  from "./Card";
+
+it("Displays Card on DOM", function() {
+  const { container, debug } = render(<Card caption="Rithm" src="School" currNum={1} totalNum={4}/>);
+  debug(container)
+
+  expect(container).toMatchSnapshot()
+  expect(container.querySelector(".Card-small")).toContainHTML("Image" )
+  expect(container.querySelector(".Card")).toContainHTML("School" )
+  expect(container.querySelector(".Card-small")).not.toContainHTML("School")
+
+});
+
